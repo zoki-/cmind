86359b0deb25f707d768412feb58f8ae5b4356c4
Zoran Bogdanovski <zoran.bogdanovski@gmail.com>
2015-11-30 21:27:07 +0100

    removed unnecessary code/renamed fragment

--- app/src/main/java/com/zoranbogdanovski/searchmoviesapp/ui/MainFragment.java
+++ app/src/main/java/com/zoranbogdanovski/searchmoviesapp/ui/MainFragment.java
@@ -19,6 +19,6 @@ public class MainFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_search, container, false);
+        return inflater.inflate(R.layout.fragment_main, container, false);
     }
 }

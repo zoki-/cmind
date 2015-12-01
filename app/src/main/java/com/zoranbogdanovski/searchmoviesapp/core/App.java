b5bfdd72c5fc766fd18dc1617e1e124cbea4b70e
Zoran Bogdanovski <zoran.bogdanovski@netcetera.com>
2015-12-01 21:18:55 +0100

    removed unused classes and resources

--- app/src/main/java/com/zoranbogdanovski/searchmoviesapp/core/App.java
+++ app/src/main/java/com/zoranbogdanovski/searchmoviesapp/core/App.java
@@ -2,10 +2,6 @@ package com.zoranbogdanovski.searchmoviesapp.core;
 
 import android.app.Application;
 
-import com.google.inject.Inject;
-import com.zoranbogdanovski.searchmoviesapp.model.configuration.Configuration;
-import com.zoranbogdanovski.searchmoviesapp.services.Services;
-
 import roboguice.RoboGuice;
 
 /**
@@ -17,13 +13,7 @@ public class App extends Application {
         RoboGuice.setUseAnnotationDatabases(false);
     }
 
-    private static final String LOG_TAG = App.class.toString();
-
     private static App instance;
-
-    @Inject
-    private Services services;
-    private Configuration configuration;
 
     /**
      * Default constructor.
@@ -54,33 +44,4 @@ public class App extends Application {
         return instance;
     }
 
-    /**
-     * Gets the services used by the app.
-     *
-     * @return the services
-     */
-    public Services getServices() {
-//        if (services == null) {
-//            services = new Services();
-//        }
-        return services;
-    }
-
-    /**
-     * Sets the configuration used by the app.
-     *
-     * @param configuration the configuration
-     */
-    public void setConfiguration(Configuration configuration) {
-        this.configuration = configuration;
-    }
-
-    /**
-     * Gets the configuration used by the app.
-     *
-     * @return the configuration
-     */
-    public Configuration getConfiguration() {
-        return configuration;
-    }
 }
